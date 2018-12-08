e54fc6b6d40f100b525d92774d9a3beba578ec1b
ryohei suzuki <ryousanngataryousann@gmail.com>
2018-12-09 02:30:18 +0900

    前に同じ

---
+++ Assets/Scripts/ResultRankItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultRankItem : MonoBehaviour
+{
+
+    [SerializeField]
+    Text text;
+
+    private void Reset()
+    {
+        text = this.GetComponentInChildren<Text>(true);
+    }
+
+    public void UpdateRankText(ResultData resultData)
+    {
+        text.text = resultData.Rank.ToString() + "位 : Player " + resultData.PlayerID.ToString() + " Score : " + resultData.Score.ToString();
+    }
+}
