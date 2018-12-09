e49c0cc9b91f4f9c9c41e8b5ef8323c4e56b2fe3
Taichi Sato <okms310gmail.com>
2018-12-09 12:52:34 +0900

    add UI

---
+++ Assets/Scripts/ButtonHoverImageChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent( typeof( Image ) )]
+public class ButtonHoverImageChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image image { get { return GetComponent<Image>(); } }
+
+	public Sprite normalImage;
+	public Sprite hoverImage;
+
+    public void OnPointerEnter( PointerEventData eventData )
+    {
+        image.sprite = hoverImage;
+        AudioPlayer.PlaySE(SEType.Cheer1);
+    }
+
+    public void OnPointerExit( PointerEventData eventData )
+    {
+		image.sprite = normalImage;
+    }
+}
