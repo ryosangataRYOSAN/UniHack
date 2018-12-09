e136567c4eacc68fa5a7681edfcded6d178df69e
Yua Akimoto <>
2018-12-09 10:11:38 +0900

    main4

--- Assets/Scripts/PlayerSellectScene.cs
+++ Assets/Scripts/PlayerSellectScene.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerSellectScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void Play1(){
+		//mainへ
+	}
+
+	public void Play2(){
+		//main２へ
+	}
+
+	public void Play3(){
+		//main３へ
+	}
+
+	public void Play4(){
+		//main４へ
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
