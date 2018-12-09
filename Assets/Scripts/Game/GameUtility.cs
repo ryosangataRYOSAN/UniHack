9da6b4ab5e3f7790ca33f3e67458a30df9780b75
Taichi Sato <okms310gmail.com>
2018-12-09 10:29:08 +0900

    feat GoGame

--- Assets/Scripts/Game/GameUtility.cs
+++ Assets/Scripts/Game/GameUtility.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GameUtility  {
-	public static void GoGameScene(bool isMaltiPlay){
-			if(!isMaltiPlay){
-				SceneLoader.LoadSceneAsync("main");
-			}else{
-				//これから
-			}
+public static class GameUtility {
+	public static void GoGameScene (int playerCount) {
+		string sceneName = "main" + playerCount.ToString ();
+		SceneLoader.LoadSceneAsync (sceneName);
+	}
+
+	public static void GoResultScene (List<ResultData> results) {
+		SceneLoader.LoadSceneAsync ("Result", new ResultScene.Options (results));
 	}
 }
